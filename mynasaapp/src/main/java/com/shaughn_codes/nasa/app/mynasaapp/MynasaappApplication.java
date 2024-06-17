8a283060611d745ccf42d6038e2d2dffad453af1
Shaughn-Codes <shaughn.p.bulgar@gmail.com>
2024-06-17 12:03:13 -0400

    setting up spring project

---
+++ mynasaapp/src/main/java/com/shaughn_codes/nasa/app/mynasaapp/MynasaappApplication.java
@@ -0,0 +1,13 @@
+package com.shaughn_codes.nasa.app.mynasaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynasaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynasaappApplication.class, args);
+	}
+
+}
---
+++ mynasaapp/src/test/java/com/shaughn_codes/nasa/app/mynasaapp/MynasaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaughn_codes.nasa.app.mynasaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynasaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
